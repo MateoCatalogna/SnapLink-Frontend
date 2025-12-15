5436b714a80cdc09ef28e732df7d5be8ea519d93
Mateo Catalogna <catalognamateo@gmail.com>
2025-12-15 16:37:51 -0300

    arreglo favicon

--- snaplink-frontend/src/app/enviroments/enviroment.ts
+++ snaplink-frontend/src/app/enviroments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://snaplink-backend-o8e5.onrender.com' // Esto lo actualizaremos en el Paso 2
+  apiUrl: 'https://snaplink-backend-o8e5.onrender.com' 
 };
